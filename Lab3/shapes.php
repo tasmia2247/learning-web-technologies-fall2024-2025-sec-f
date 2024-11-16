c24c29119e9af9488258401b48e11b7cc3d0e0cf
tasmia2247 <kabirtasmia10@gmail.com>
2024-11-16 11:59:28 +0600

    Create shapes.php

---
+++ Lab3/shapes.php
@@ -0,0 +1,59 @@
+<?php
+
+$i=0;$a=0;
+$j=10;$b=0;
+for($i=0;$i<=10;$i++){
+
+  
+
+     for($a=0;$a<=$i;$a++){
+
+    echo"*";   
+    }
+    echo"<br>";
+}
+echo"<br>";
+
+
+
+//$j=10;$b=0;
+$num=1;
+for($j=10;$j>=0;$j--){
+
+  
+    echo"<br>";
+     for($b=$j;$b>=$j;$j--){
+
+    echo"$num";  
+    $num++;
+   
+    }
+    
+    echo"<br>";
+}
+echo"<br>";
+
+
+
+$Char = 'A';
+ 
+$k=0;$c=0;
+
+for($k=0;$k<=10;$k++){
+
+  
+
+     for($c=0;$c<=$k;$c++){
+
+    echo"$Char"; 
+    $Char++  
+     }
+    echo"<br>";
+}
+echo"<br>";
+ 
+ 
+ 
+ 
+ 
+?>
